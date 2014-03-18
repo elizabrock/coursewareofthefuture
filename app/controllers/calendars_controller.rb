d8e1b1b9b612e7608ded5f012bd4427fb8bb7306
Jerry Sims <jerrysims07@gmail.com>
2014-03-17 21:07:01 -0500

    student now sees 'today' on the course calendar

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,3 +1,8 @@
 class CalendarsController < ApplicationController
   expose(:events){ course.events }
+
+  def show
+    @event = course.events.new(summary: "Today", date: Time.now.strftime("%Y-%m-%d"))
+  end
+
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Event do
   it { should belong_to :course }
+
 end
