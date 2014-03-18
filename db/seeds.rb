b3d4b1bf3697932346f71cb3fe662bbcfbec379f
Jerry Sims <jerrysims07@gmail.com>
2014-03-17 21:07:01 -0500

    student now sees 'today' on the course calendar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Fabricate(:active_course)
-course = Course.new(:start_date "2014-03-15", :end_date "2014-06-15", :syllabus "test syllabus", :title "Seed Course", :active_course true)
+course = Fabricate(:active_course)
 course.save
