662a1ead7b55e48dbd4de746f902a62e02a566ef
Eliza Brock <eliza@elizabrocksoftware.com>
2014-03-26 12:16:21 -0500

    Simplify HomeController#index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,17 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate!
 
   def index
-    if current_user
-      if current_user.instructor?
-        if Course.count == 1
-          redirect_to Course.first
-        else
-          redirect_to courses_path
-        end
-      elsif current_user.courses.empty?
-        redirect_to new_enrollment_path
+    return unless current_user
+    if current_user.instructor?
+      if Course.count == 1
+        redirect_to Course.first
       else
-        redirect_to course_path(current_user.courses.first)
+        redirect_to courses_path
       end
+    elsif current_user.courses.empty?
+      redirect_to new_enrollment_path
+    else
+      redirect_to course_path(current_user.courses.first)
     end
   end
 end
