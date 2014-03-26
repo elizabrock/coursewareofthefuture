dc4ba808f22e0860dcb710cce39ec06db030425b
Eliza Brock <eliza@elizabrocksoftware.com>
2014-03-26 12:12:01 -0500

    Follow the coveralls instructions better ;)

--- lib/test_coverage_helper.rb
+++ lib/test_coverage_helper.rb
@@ -1,7 +1,6 @@
 require 'simplecov'
 require 'coveralls'
 
-formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
 
 SimpleCov.start 'rails'
