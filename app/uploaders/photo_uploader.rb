f02970b2ebdc558aafaf8267c5d6de8410c83818
Eliza Brock <eliza@elizabrocksoftware.com>
2014-04-04 12:21:15 -0500

    Remove manual file setting in uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,9 +6,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -41,11 +38,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
 end
