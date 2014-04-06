f360c4409d31d73a998ace161ab5bae7838442f7
Eliza Brock <eliza@elizabrocksoftware.com>
2014-04-06 12:17:06 -0500

    Revert "Remove extension whitelist, as further heroku debugging effort."
    This reverts commit e40d89c3602292230ec43c2b2af29f8d1a70c9cc.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -35,7 +35,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
