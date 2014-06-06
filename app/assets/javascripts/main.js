43b7fcc9d3cf24c2dd21e36b61729924e0f2e41e
Giovanni Delacqua <giovanni.delacqua@gmail.com>
2014-06-06 08:02:45 -0500

    more changes to tabs interface

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,30 +11,30 @@
   }
 
   function show1(){
-    $('#upcoming_material').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, rgb(214, 251, 252) 0%, rgb(148, 203, 206) 100%)').css('border-top', '1px solid').css('border-bottom', '1px solid').css('border-left', '1px solid');
-    $('#material_to_cover').css('background', 'none').css('border', 'none');
-    $('#material_covered').css('background', 'none').css('border', 'none');
+    $('#upcoming_material').css('background', 'none').css('border', 'none');
+    $('#material_to_cover').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', 'none').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#covered_materials').hide();
     $('#material_to_cover_yet').hide();
-    $('#upcoming_materials').css('display', 'block').css('background', '-webkit-radial-gradient(center, ellipse, rgba(214,251,252,1) 0%,rgba(148,203,206,1) 100%)');//.css('background', '-webkit-linear-gradient(-45deg, #e4f5fc 0%,#bfe8f9 50%,#9fd8ef 96%,#9fd8ef 96%,#2ab0ed 100%)').css('background-size', 'cover');
+    $('#upcoming_materials').fadeIn(500).css('display', 'block');
   }
 
   function show2(){
-    $('#material_to_cover').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, rgb(214, 251, 252) 0%, rgb(148, 203, 206) 100%)').css('border-top', '1px solid').css('border-bottom', '1px solid').css('border-left', '1px solid');
-    $('#upcoming_material').css('background', 'none').css('border', 'none');
-    $('#material_covered').css('background', 'none').css('border', 'none');
+    $('#material_to_cover').css('background', 'none').css('border', 'none');
+    $('#upcoming_material').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#upcoming_materials').hide();
     $('#covered_materials').hide();
-    $('#material_to_cover_yet').css('display', 'block').css('background', '-webkit-radial-gradient(center, ellipse, rgba(214,251,252,1) 0%,rgba(148,203,206,1) 100%)');//.css('background', '-webkit-linear-gradient(-45deg, #e4f5fc 0%,#bfe8f9 50%,#9fd8ef 96%,#9fd8ef 96%,#2ab0ed 100%)').css('background-size', 'cover');
+    $('#material_to_cover_yet').fadeIn(500).css('display', 'block');
   }
 
   function show3(){
-    $('#material_covered').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, rgb(214, 251, 252) 0%, rgb(148, 203, 206) 100%)').css('border-top', '1px solid').css('border-bottom', '1px solid').css('border-left', '1px solid');
-    $('#upcoming_material').css('background', 'none').css('border', 'none');
-    $('#material_to_cover').css('background', 'none').css('border', 'none');
+    $('#material_covered').css('background', 'none').css('border', 'none');
+    $('#material_to_cover').css('border-bottom', 'none').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#upcoming_material').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#upcoming_materials').hide();
     $('#material_to_cover_yet').hide();
-    $('#covered_materials').css('display', 'block').css('background', '-webkit-radial-gradient(center, ellipse, rgba(214,251,252,1) 0%,rgba(148,203,206,1) 100%)');//.css('background', '-webkit-linear-gradient(-45deg, #e4f5fc 0%,#bfe8f9 50%,#9fd8ef 96%,#9fd8ef 96%,#2ab0ed 100%)').css('background-size', 'cover');
+    $('#covered_materials').fadeIn(500).css('display', 'block');
   }
 
 })();
