58ec534f31116be1254f3058a2ab6a18438d49e0
JamesNix <jamesfnix@gmail.com>
2014-06-05 21:55:36 -0500

    Added covered materials validation

--- app/models/covered_material.rb
+++ app/models/covered_material.rb
@@ -1,9 +1,11 @@
 class CoveredMaterial < ActiveRecord::Base
   belongs_to :course
   validates_presence_of :covered_on
+  validates :material_fullpath, uniqueness: { scope: :course,
+    message: "should be covered once per course" }
 
   default_scope ->{ order("covered_on ASC, id ASC") }
-  before_validation :set_covered_on, on: :create
+  after_validation :set_covered_on, on: :create
 
   def fullpath
     material_fullpath.gsub("materials/","")
