b4aa830976d11b4f33faca6c033cefa126ff4fe2
Giovanni Delacqua <giovanni.delacqua@gmail.com>
2014-06-06 10:00:30 -0500

    gradients removed and material alligned with page layout

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,8 +12,8 @@
 
   function show1(){
     $('#upcoming_material').css('background', 'none').css('border', 'none');
-    $('#material_to_cover').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', 'none').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
-    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_to_cover').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', 'none').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#covered_materials').hide();
     $('#material_to_cover_yet').hide();
     $('#upcoming_materials').fadeIn(500).css('display', 'block');
@@ -21,8 +21,8 @@
 
   function show2(){
     $('#material_to_cover').css('background', 'none').css('border', 'none');
-    $('#upcoming_material').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
-    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#upcoming_material').css('border-bottom', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_covered').css('border-top', '1px solid').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#upcoming_materials').hide();
     $('#covered_materials').hide();
     $('#material_to_cover_yet').fadeIn(500).css('display', 'block');
@@ -30,8 +30,8 @@
 
   function show3(){
     $('#material_covered').css('background', 'none').css('border', 'none');
-    $('#material_to_cover').css('border-bottom', 'none').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
-    $('#upcoming_material').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#material_to_cover').css('border-bottom', 'none').css('border-right', '1px solid').css('border-left', '1px solid').css('border-top', '1px solid').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
+    $('#upcoming_material').css('border-right', '1px solid').css('border-left', '1px solid').css('border-bottom', '1px solid').css('background', 'gainsboro');//.css('background', '-webkit-radial-gradient(50% 50%, ellipse cover, #d6fbfc 0%, #94cbce 100%)');
     $('#upcoming_materials').hide();
     $('#material_to_cover_yet').hide();
     $('#covered_materials').fadeIn(500).css('display', 'block');
