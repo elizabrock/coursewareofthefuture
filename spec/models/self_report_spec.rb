50d5e31440f19fb382fd214ad721abb9a9e624c2
Nat Webb <natrwebb@gmail.com>
2014-06-08 19:05:46 -0500

    :zap::octopus:self_report spec tests date uniqueness validation:zap:

--- spec/models/self_report_spec.rb
+++ spec/models/self_report_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe SelfReport do
   it { should validate_presence_of :user }
   it { should validate_presence_of :date }
+  it { should validate_uniqueness_of :date }
 
   describe ".send_student_reminders!" do
     def do_action
