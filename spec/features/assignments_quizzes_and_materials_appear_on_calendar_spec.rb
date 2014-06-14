64fcf84e2a5447c17921e93888730fdffce823fc
Eliza Brock <eliza@elizabrocksoftware.com>
2014-06-14 11:26:11 -0500

    Fix: Only one event per day shows up on the class calendar
    resolves #69

--- spec/features/assignments_quizzes_and_materials_appear_on_calendar_spec.rb
+++ spec/features/assignments_quizzes_and_materials_appear_on_calendar_spec.rb
@@ -16,21 +16,37 @@ feature "Assignments quizzes and materials appear on calendar" do
     Fabricate(:milestone, title: "Pt. 1", deadline: "2014/01/02", assignment: published)
     Fabricate(:milestone, title: "Pt. 2", deadline: "2014/01/04", assignment: published)
 
-    visit root_path
-    click_link "Course Calendar"
+    visit course_calendar_path(course)
     page.should have_calendar_entry("2014-01-02", text: "Koans: Pt. 1 Due")
     page.should have_calendar_entry("2014-01-04", text: "Koans: Pt. 2 Due")
     page.should_not have_content "Unpublished Assignment"
     page.should_not have_content "Pt. n"
   end
 
+  scenario "Fix: Multiple milestones on a day" do
+    published = Fabricate(:published_assignment, title: "Koans", course: course)
+    Fabricate(:milestone, title: "Pt. 1", deadline: "2014/01/02", assignment: published)
+    Fabricate(:milestone, title: "Pt. 2", deadline: "2014/01/02", assignment: published)
+
+    visit course_calendar_path(course)
+    page.should have_calendar_entry("2014-01-02", text: "Koans: Pt. 1 Due")
+    page.should have_calendar_entry("2014-01-02", text: "Koans: Pt. 2 Due")
+  end
+
   scenario "Quizzes" do
     Fabricate(:quiz, title: "Baseline Knowledge", deadline: "2014/01/01", course: course)
     Fabricate(:quiz, title: "Midpoint Checkin", deadline: "2014/01/15", course: course)
-    visit root_path
-    click_link "Course Calendar"
+    visit course_calendar_path(course)
     page.should have_calendar_entry("2014-01-01", text: "Baseline Knowledge Due")
     page.should have_calendar_entry("2014-01-15", text: "Midpoint Checkin Due")
+  end
+
+  scenario "Fix: Multiple quizzes on a day" do
+    Fabricate(:quiz, title: "Baseline Knowledge", deadline: "2014/01/01", course: course)
+    Fabricate(:quiz, title: "Midpoint Checkin", deadline: "2014/01/01", course: course)
+    visit course_calendar_path(course)
+    page.should have_calendar_entry("2014-01-01", text: "Baseline Knowledge Due")
+    page.should have_calendar_entry("2014-01-01", text: "Midpoint Checkin Due")
   end
 
   scenario "Covered Materials" do
@@ -40,10 +56,20 @@ feature "Assignments quizzes and materials appear on calendar" do
     Fabricate(:covered_material,
               material_fullpath: "materials/computer-science/logic/set_theory.md",
               covered_on: "2014/01/20", course: course)
-    visit root_path
-    click_link "Course Calendar"
-
+    visit course_calendar_path(course)
     page.should have_calendar_entry("2014-01-15", text: "Logic")
     page.should have_calendar_entry("2014-01-20", text: "Set Theory")
+  end
+
+  scenario "Fix: multiple covered materials on a day" do
+    Fabricate(:covered_material,
+              material_fullpath: "materials/computer-science/logic/logic.md",
+              covered_on: "2014/01/15", course: course)
+    Fabricate(:covered_material,
+              material_fullpath: "materials/computer-science/logic/set_theory.md",
+              covered_on: "2014/01/15", course: course)
+    visit course_calendar_path(course)
+    page.should have_calendar_entry("2014-01-15", text: "Logic")
+    page.should have_calendar_entry("2014-01-15", text: "Set Theory")
   end
 end
