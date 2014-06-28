ba7eb3981bf23a487406b26ff02ca9a62d6c6fa2
Eliza Brock <eliza@elizabrocksoftware.com>
2014-06-28 08:51:56 -0500

    Repair migration

--- db/migrate/20140621175940_convert_material_fullpaths.rb
+++ db/migrate/20140621175940_convert_material_fullpaths.rb
@@ -1,19 +1,20 @@
-class MyCoveredMaterial < ActiveRecord::Base
-  self.table_name = :covered_materials
-end
-class MyPrerequisites < ActiveRecord::Base
-  self.table_name = :prerequisites
-end
-class MyReadMaterial < ActiveRecord::Base
-  self.table_name = :read_materials
-end
-
 class ConvertMaterialFullpaths < ActiveRecord::Migration
+  class MyCoveredMaterial < ActiveRecord::Base
+    self.table_name = "covered_materials"
+  end
+  class MyPrerequisites < ActiveRecord::Base
+    self.table_name = "prerequisites"
+  end
+  class MyReadMaterial < ActiveRecord::Base
+    self.table_name = "read_materials"
+  end
   def up
     materialishes = (MyCoveredMaterial.all + MyPrerequisites.all + MyReadMaterial.all).flatten
-    materialishes.each do |materialish|
-      materialish.material_fullpath.gsub!("materials/","")
-      materialish.save!
+    materialishes.each do |m|
+      if m.material_fullpath.present?
+        m.material_fullpath = m.material_fullpath.gsub("materials/","")
+        m.save!
+      end
     end
   end
 end
